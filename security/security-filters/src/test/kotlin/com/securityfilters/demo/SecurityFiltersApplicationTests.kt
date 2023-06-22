426d73b4e314a3123a0ab025b05bfdb8a6e3710c
Georgi Velikov <99119321+georgivelikov-devsearch@users.noreply.github.com>
2023-06-22 20:31:59 +0300

    Initial

---
+++ security/security-filters/src/test/kotlin/com/securityfilters/demo/SecurityFiltersApplicationTests.kt
@@ -0,0 +1,14 @@
+package com.security.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SecurityFiltersApplicationTests {
+
+	@Test
+	fun contextLoads() {
+
+	}
+
+}
